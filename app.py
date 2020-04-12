bd736910cdd6f6065b3dae1a2ff5419f71334e83
tobias <tobias@data.gov.sg>
2020-04-12 19:33:30 +0800

    Changed to use subplots mechanism and plotly API directly

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -11,6 +13,69 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 df = pd.read_csv('data/enigma-jhu/apr-10-data.csv')
 df['datetime'] = pd.to_datetime(df['last_update'])
 
+# plots
+fig_subplots = make_subplots(
+    rows=1, 
+    cols=2,
+    subplot_titles = ("Confirmed vs Recovered", "Deaths vs Recovered")
+)
+
+# Plot 1 - Confirmed vs Recovered
+fig_subplots.append_trace(
+    go.Scatter(
+        x=df['datetime'],
+        y=df['confirmed'],
+        mode='lines+markers',
+        name="Confirmed",
+        line={
+            'color':'firebrick'
+            }
+        ),
+        row = 1,
+        col = 1
+)
+fig_subplots.append_trace(
+    go.Scatter(
+        x=df['datetime'],
+        y=df['recovered'],
+        mode='lines+markers',
+        name="Recovered",
+        line={
+            'color':'royalblue'
+            }
+        ),
+        row=1,
+        col=1
+)
+
+# Plot 2 - Deaths vs Recovered
+fig_subplots.append_trace(
+    go.Scatter(
+        x=df['datetime'],
+        y=df['deaths'],
+        mode='lines+markers',
+        name="Deaths",
+        line={
+            'color':'black'
+            }
+        ),
+        row = 1,
+        col = 2
+)
+fig_subplots.append_trace(
+    go.Scatter(
+        x=df['datetime'],
+        y=df['recovered'],
+        mode='lines+markers',
+        name="Recovered",
+        line={
+            'color':'royalblue'
+            }
+        ),
+        row = 1,
+        col = 2
+)
+
 app.layout = html.Div(children=[
     html.H1(children='Ministry of Health COVID-19 Dashboard'),
 
@@ -19,68 +84,9 @@ app.layout = html.Div(children=[
     '''),
 
     dcc.Graph(
-        id='Confirmed vs Recovered',
-        figure={
-            'data': [
-                {
-                    'x': df['datetime'], 
-                    'y': df['confirmed'], 
-                    'type': 'line', 
-                    'name': 'Confirmed', 
-                    'mode':'lines+markers', 
-                    'line':{
-                        'color':'royalblue'
-                        }
-                    },
-                {
-                    'x': df['datetime'], 
-                    'y': df['recovered'], 
-                    'type': 'line', 
-                    'name': 'Recovered', 
-                    'mode':'lines+markers',
-                    'line':{
-                        'color':'pink'
-                    }
-                    },
-            ],
-            'layout': {
-                'title': 'Confirmed vs Recovered'
-            }
-        }
+        id='Confirmed, Recovered and Deaths',
+        figure=fig_subplots
     ),
-
-    dcc.Graph(
-        id='Recovered vs Deaths',
-        figure={
-            'data': [
-                {
-                    'x': df['datetime'], 
-                    'y': df['deaths'], 
-                    'type': 'line', 
-                    'name': 'Deaths', 
-                    'mode':'lines+markers', 
-                    'line':{
-                        'color':'black'
-                        }
-                    },
-                {
-                    'x': df['datetime'], 
-                    'y': df['recovered'], 
-                    'type': 'line', 
-                    'name': 'Recovered', 
-                    'mode':'lines+markers',
-                    'line':{
-                        'color':'pink'
-                    }},
-            ],
-            'layout': {
-                'title': 'Deaths vs Recovered'
-            }
-        }
-    ),
-
-
-    
 ])
 
 if __name__ == '__main__':
